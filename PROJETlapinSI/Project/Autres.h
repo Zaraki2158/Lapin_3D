70b14b0b41926a19490a7fb0a416c5cd0bcc62cb
Zaraki2158 <162483227+Zaraki2158@users.noreply.github.com>
2024-04-22 09:16:51 +0200

    Add files via upload

---
+++ PROJETlapinSI/Project/Autres.h
@@ -0,0 +1,17 @@
+class Point
+{
+    public :
+	//coordonnées x, y et z du point
+	double x;
+	double y;
+	double z;
+	// couleur r, v et b du point
+	float r;
+	float g;
+	float b;
+};
+
+  struct Face
+  {
+    int i, j, k, l;
+};
